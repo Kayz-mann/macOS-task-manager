d7c3c965dd1a60d7ae8d2b627eb5e48feafc51d2
Kayz-mann <kayodefredrickbalogun@gmail.com>
2025-07-30 01:22:13 +0100

    Initial Commit

---
+++ taskmanager/taskmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskmanagerApp.swift
+//  taskmanager
+//
+//  Created by Balogun Kayode on 30/07/2025.
+//
+
+import SwiftUI
+
+@main
+struct taskmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ taskmanagerTests/taskmanagerTests.swift
@@ -0,0 +1,17 @@
+//
+//  taskmanagerTests.swift
+//  taskmanagerTests
+//
+//  Created by Balogun Kayode on 30/07/2025.
+//
+
+import Testing
+@testable import taskmanager
+
+struct taskmanagerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
